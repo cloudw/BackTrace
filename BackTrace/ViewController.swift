58a375c7e3b49907ee602620a179eb63a73665d1
Yun Wu <15059225+cloudw@users.noreply.github.com>
2018-04-02 21:07:02 -0500

    Create project

---
+++ BackTrace/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BackTrace
+//
+//  Created by Yun Wu on 4/2/18.
+//  Copyright © 2018 Yun Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ BackTraceTests/BackTraceTests.swift
@@ -0,0 +1,36 @@
+//
+//  BackTraceTests.swift
+//  BackTraceTests
+//
+//  Created by Yun Wu on 4/2/18.
+//  Copyright © 2018 Yun Wu. All rights reserved.
+//
+
+import XCTest
+@testable import BackTrace
+
+class BackTraceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
